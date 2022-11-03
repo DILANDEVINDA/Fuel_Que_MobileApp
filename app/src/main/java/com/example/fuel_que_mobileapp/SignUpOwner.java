1f3b8b333a8e28047f73110b248f86d1b306a54b
DILANDEVINDA <68731903+DILANDEVINDA@users.noreply.github.com>
2022-11-03 06:32:23 +0530

    signup works

--- app/src/main/java/com/example/fuel_que_mobileapp/SignUpOwner.java
+++ app/src/main/java/com/example/fuel_que_mobileapp/SignUpOwner.java
@@ -21,7 +21,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SignUpOwner extends AppCompatActivity {
 
-    EditText Name,Email,Password,MNumber,SName,Location,Remaining92,Remaining95,RemainingAutoDiesel,RemainingSuperDiesel;
+    EditText Name,Email,Password,MNumber,SName,Location,RemainingNinetyTwo,RemainingNinetyTfive,RemainingAutoDiesel,RemainingSuperDiesel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +34,8 @@ public class SignUpOwner extends AppCompatActivity {
         MNumber = findViewById(R.id.editTextMNumberOfOwner);
         SName = findViewById(R.id.editTextFStationNameOfOwner);
         Location = findViewById(R.id.editTextSLocationOfOwner);
-        Remaining92 = findViewById(R.id.editTextRemainingFuel92);
-        Remaining95 = findViewById(R.id.editTextRemainingFuel95);
+        RemainingNinetyTwo = findViewById(R.id.editTextRemainingFuel92);
+        RemainingNinetyTfive = findViewById(R.id.editTextRemainingFuel95);
         RemainingAutoDiesel = findViewById(R.id.editTextRemainingFuelAutoDiesel);
         RemainingSuperDiesel = findViewById(R.id.editTextRemainingFuelSuperDiesel);
         Button signup = findViewById(R.id.btnSignUPForOwner);
@@ -43,42 +43,47 @@ public class SignUpOwner extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                UserModel user = new UserModel(Name.getText().toString(),Email.getText().toString(),Password.getText().toString(),MNumber.getText().toString(),"Owner");
-                OwnerAndStationModel oandS = new OwnerAndStationModel(user,SName.getText().toString(),Location.getText().toString(),Integer.getInteger(Remaining92.getText().toString()),Integer.getInteger(Remaining95.getText().toString()),Integer.getInteger(RemainingAutoDiesel.getText().toString()),Integer.getInteger(RemainingSuperDiesel.getText().toString()));
+                    UserModel user = new UserModel(Name.getText().toString(),Email.getText().toString(),Password.getText().toString(),MNumber.getText().toString(),"Owner");
+                    OwnerAndStationModel oandS = new OwnerAndStationModel(user,SName.getText().toString(),Location.getText().toString(),
+                            Integer.parseInt(RemainingNinetyTwo.getText().toString()),
+                            Integer.parseInt(RemainingNinetyTfive.getText().toString()),
+                            Integer.parseInt(RemainingAutoDiesel.getText().toString()),
+                            Integer.parseInt(RemainingSuperDiesel.getText().toString()));
 
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("http://192.168.1.6:45455/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
+                    Log.d("fuel status", RemainingNinetyTwo.getText().toString() + " " + RemainingNinetyTfive.getText().toString() + " " + RemainingAutoDiesel.getText().toString() + " " + RemainingSuperDiesel.getText().toString());
 
-                UserAPI api = retrofit.create(UserAPI.class);
-                Call<Void> call = api.createOwner(
-                        oandS);
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("http://192.168.1.6:45455/")
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
 
-                call.enqueue(new Callback<Void>() {
+                    UserAPI api = retrofit.create(UserAPI.class);
+                    Call<Void> call = api.createOwner(
+                            oandS);
 
-                    @Override
-                    public void onResponse(Call<Void> call, Response<Void> response) {
-                        if(response.code() == 200){
-                            Log.d("response","working");
-                            Intent i = new Intent(SignUpOwner.this, Login.class);
-                            startActivity(i);
-                            finish();
-                        }else{
-                            Log.d("response","not working");
-                            Intent i = new Intent(SignUpOwner.this, Login.class);
-                            startActivity(i);
-                            finish();
+                    call.enqueue(new Callback<Void>() {
 
+                        @Override
+                        public void onResponse(Call<Void> call, Response<Void> response) {
+                            if(response.code() == 200){
+                                Log.d("response","working");
+                                Intent i = new Intent(SignUpOwner.this, Login.class);
+                                startActivity(i);
+                                finish();
+                            }else{
+                                Log.d("response","not working");
+                                Intent i = new Intent(SignUpOwner.this, Login.class);
+                                startActivity(i);
+                                finish();
+
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call<Void> call, Throwable t) {
-                        Log.d("API failed : ",t.getMessage().toString());
-                    }
-
-                });
+                        @Override
+                        public void onFailure(Call<Void> call, Throwable t) {
+                            Log.d("API failed : ",t.getMessage().toString());
+                        }
+                    });
             }
         });
     }
